c0d821b5b059d1e5d50a2876049dc6cd8fa39e84
danielglez4234 <gnzlzdaniel.task@gmail.com>
2019-11-19 18:30:45 +0000

    first changes - Part II step 9

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,61 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game{
+   constructor(){
+    this.missed = 0;
+    this.phrases = this.createPhrases();
+    this.activePhrase = `null`;
+  }
+  // * Creates phrases for use in game
+  // * @return {array} An array of phrases that could be used in the game
+  createPhrases(){
+    const phrasesArray = [
+      {phrase: 'May the force be with you'},
+      {phrase: 'The stuff that dreams are made of'},
+      {phrase: 'Show me the money this is a robbery'},
+      {phrase: 'Tomorrow is another day'},
+      {phrase: 'Houston we have a problem'}
+    ];
+    return phrasesArray;
+  }
+
+  // * Selects random phrase from phrases property
+  // * @return {Object} Phrase object chosen to be used
+  getRandomPhrase(){
+    const phraseArray = this.phrases;
+    const randomPhrase = phraseArray[Math.floor(Math.random() * phraseArray.length)].phrase;
+    return randomPhrase;
+  };
+
+  // * Begins game by selecting a random phrase and displaying it to user
+  startGame(){
+    $('#overlay').slideUp(1000);
+    const activePhrase = this.getRandomPhrase();
+    const phrase = new Phrase(activePhrase);
+    phrase.addPhraseToDisplay();
+    this.activePhrase = activePhrase;
+  };
+
+  // * Checks for winning move
+  // * @return {boolean} True if game has been won, false if game wasn't won
+  checkForWin(){
+
+  };
+
+  // * Increases the value of the missed property
+  // * Removes a life from the scoreboard
+  // * Checks if player has remaining lives and ends game if player is out
+  removeLife(){
+
+  };
+
+  // * Displays game over message
+  // * @param {boolean} gameWon - Whether or not the user won the game
+  gameOver(gameWon){
+
+  };
+
+
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,41 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase{
+   constructor(phrase){
+    this.phrase = phrase.toLowerCase();
+  }
+
+  // * Display phrase on game board
+  addPhraseToDisplay(){
+    const theActualPhrase = this.phrase;
+    const $ul = $('#phrase ul');
+    for (var i = 0; i < theActualPhrase.length; i++) {
+      const $li = $('<li class="hide letter '+ theActualPhrase.charAt(i) +'">'+ theActualPhrase.charAt(i) +'</li>');
+      const $liSpace = $('<li class="hide space" display="none"></li>');
+      if (theActualPhrase.charAt(i) != ' ') {
+        $ul.append($li);
+      }else {
+        $ul.append($liSpace);
+      }
+    }
+  }
+
+
+  // * Checks if passed letter is in phrase
+  // * @param (string) letter - Letter to check
+  checkLetter(letter){
+    var matchLetter = this.phrase.includes(letter);
+    return matchLetter;
+  };
+
+  // * Displays passed letter on screen after a match is found
+  // * @param (string) letter - Letter to display
+  showMatchedLetter(letter){
+
+  };
+
+
+
+ }
